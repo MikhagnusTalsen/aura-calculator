2c32b76380ebd0beb4ac926ea70b2cd03ea092e6
MikhagnusTalsen <ashutoshdpps@gmail.com>
2025-05-31 02:51:24 +0530

    a few changes

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    let height = event.target.height.value;
-    let weight = event.target.weight.value;
-    let looks = event.target.looks.value;
-    let networth = event.target.networth.value;
-    let aura = Math.floor(((weight / (height / 100) ** 2)));
+    const height = (event.target.height.value);
+    const weight = (event.target.weight.value);
+    const looks = (event.target.looks.value);
+    const networth = (event.target.networth.value);
 
-    if (aura < 18.5) {
-        aura *= looks ** 2.71;
-    } else if (aura >= 18.5 && aura < 24.9) {
-        aura *= looks ** 3.14;
-    } else if (aura >= 25 && aura < 27) {
-        aura *= looks ** 1.23;
-    } else {
-        aura *= looks ** 0.7;
+    if (weight <= 0 || isNaN(weight)) {
+        let err = document.getElementById("weighterror");
+        err.textContent = "Enter a valid weight";
+        err.style.display = "block";
     }
+    else if (height <= 0 || isNaN(height)) {
+        let err = document.getElementById("heighterror");
+        err.textContent = "Enter a valid height";
+        err.style.display = "block";
+    }
+    else if (looks < 0 || looks > 10 || isNaN(looks)) {
+        let err = document.getElementById("looksterror");
+        err.textContent = "Enter valid looks";
+        err.style.display = "block";
+    }
+    else if (networth < 0 || isNaN(networth)) {
+        let err = document.getElementById("networtherror");
+        err.textContent = "Enter a valid networth";
+        err.style.display = "block";
 
-    aura = Math.floor(aura) + Number(networth);
+    } else {
+        let aura = Math.floor(((weight / (height / 100) ** 2)));
 
+        if (aura < 18.5) {
+            aura *= looks ** 2.71;
+        } else if (aura >= 18.5 && aura < 24.9) {
+            aura *= looks ** 3.14;
+        } else if (aura >= 25 && aura < 27) {
+            aura *= looks ** 1.23;
+        } else {
+            aura *= looks ** 0.7;
+        }
 
+        aura = Math.floor(aura) + Number(networth);
 
-    let reuslt = document.querySelector("#result");
-    form.style.display = "none"
-    reuslt.style.display = "block";
-    reuslt.textContent = `Your aura is ${aura}`;
-
-
+        let result = document.querySelector("#result");
+        form.style.display = "none"
+        result.style.display = "block";
+        result.textContent = `Your aura is ${aura}`;
+    }
 })
